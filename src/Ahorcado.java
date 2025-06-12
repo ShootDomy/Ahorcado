bea18325d1f2ead9094c6d25ffc409fe06c7cd76
ShootDomy <canizaresdomenica4@gmail.com>
2025-06-12 15:58:19 -0500

    Add initial project setup with settings and main class implementation

---
+++ src/Ahorcado.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Ahorcado {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Sacanner(System.in);
+
+        String palabraSecreta = "Gato";
+
+        sc.close();
+    }
+}
